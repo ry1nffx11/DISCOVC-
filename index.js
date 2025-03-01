f787b7b2f30513e2200e8a89c6fb698db14f646e
itachimaru <depressive.suicidal.bm@gmail.com>
2025-03-01 22:43:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('events').EventEmitter.prototype._maxListeners = 50;
 const { Client } = require('discord.js-selfbot-v13');
 const { joinVoiceChannel } = require('@discordjs/voice');
 
-const channels = ['1344983405284167680'];
+const channels = ['1345416186749521941'];
 
 const checkAndJoin = async (client, channelId) => {
   try {
