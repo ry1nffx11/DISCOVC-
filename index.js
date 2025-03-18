52119b0a2f8b9a49704609d56639991200871336
ry1nffx11 <141871743+ry1nffx11@users.noreply.github.com>
2025-03-18 07:00:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('events').EventEmitter.prototype._maxListeners = 50;
 const { Client } = require('discord.js-selfbot-v13');
 const { joinVoiceChannel } = require('@discordjs/voice');
 
-const channels = ['1351220340101222542'];
+const channels = ['1351449415788920874'];
 
 const checkAndJoin = async (client, channelId) => {
   try {
